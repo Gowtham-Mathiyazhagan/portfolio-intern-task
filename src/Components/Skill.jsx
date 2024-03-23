8b631b6831185db6b24750a4b0fd3247e135e140
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-03-23 12:52:52 +0530

    Skill component and style implemented

---
+++ src/Components/Skill.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Skill = ({ skills }) => {
+  return (
+    <>
+	<article className="resume active">
+      <header>
+        <h2 className="h2 article-title">Skills</h2>
+      </header>
+      <section className="skill">
+        <h3 className="h3 skills-title">My skills</h3>
+
+        <ul className="skills-list content-card">
+         {
+			skills.map(val=>{
+				return(
+					<li key={val.name} className="skills-item">
+					<div className="title-wrapper">
+						<img src={val.image.url} alt={val.name} />
+					  <h5 className="h5">{val.name}</h5>
+					  <data value={val.percentage}>{val.percentage}%</data>
+					</div>
+		
+					<div className="skill-progress-bg">
+					  <div className="skill-progress-fill" style={{width: `${val.percentage}%`}}></div>
+					</div>
+				  </li>
+				)
+			})
+		 }
+        </ul>
+      </section>
+	</article>
+    </>
+  );
+};
+
+export default Skill;
